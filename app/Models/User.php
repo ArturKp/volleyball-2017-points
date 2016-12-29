81e0fc67a575667de5d70f6635c3df72bd39e924
Artur Käpp <arturkp@gmail.com>
2016-12-29 15:17:49 +0200

    Login fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,12 @@
 namespace App\Models;
 
 use App\Models\BaseModel;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends BaseModel implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract {
